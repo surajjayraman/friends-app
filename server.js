23418bbf3beb3ac2d2179eb68ddccc1143879a2d
surajjayraman <suraj.jayraman@gmail.com>
2023-10-29 09:21:22 -0400

    Update express app with dummy data

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+let data = [
+    { "uid": "10f4dc3d89bb", "name": "Ali Gator" },
+    { "uid": "b16f63101776", "name": "Sue Permann" },
+    { "uid": "337e39bccc6d", "name": "Rose Gardner" },
+    { "uid": "ee7cd570992d", "name": "Macon Paine" },
+    { "uid": "e2cae1a21b70", "name": "Wanda Rinn" }
+];
+
 const express = require('express');
 
 const PORT = 8080;
