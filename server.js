07c5debaa4389c109c3e52ffb250c097f66399dd
surajjayraman <suraj.jayraman@gmail.com>
2023-10-29 08:47:50 -0400

    Update express app with names endpoint

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
+app.get('/names', (req, res) => {
+    res.send('Welcome to the Friends App')
+
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT} with nodemon up`)
 });
